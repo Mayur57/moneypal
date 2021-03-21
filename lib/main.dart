97e03385376ea784b6adc42c31bdc6a65ae1fab5
Mayur Bhoi <mayur072000@gmail.com>
2021-03-22 03:25:27 +0530

    Added Fonts (Gilroy Font Family)

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'moneypal',
-      initialRoute: '/',
+      initialRoute: '/login',
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
 
--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -9,6 +9,27 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            children: [
+
+              SizedBox(
+                height: 50,
+              ),
+
+              Image.asset(
+                "assets/images/logo_text_long.png",
+                width: deviceWidth / 2.1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
